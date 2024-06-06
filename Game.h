6a35107ef0258dac67d4cd5c36552e10e6d5f786
lam_edora <lam_edora@LAMEDORA>
2024-06-06 15:49:51 +0700

    Add project files.

---
+++ Game.h
@@ -0,0 +1,39 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#include "Common_Funtions.h"
+
+class BaseObject;
+class Background;
+class Bird;
+class Pipe;
+class Menu;
+
+class Game {
+private:
+	SDL_Window* window;
+	SDL_Renderer* ren;
+
+	bool isRunning;
+
+	BaseObject* background;
+
+	Bird* bird;
+	Menu* menu;
+
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int weidth, int hight);
+	void update();
+	void render();
+	void close();
+
+	void handleEvent();
+
+	bool Running();
+
+};
+
+#endif // !GAME_H_
